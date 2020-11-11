96727fed4d855f8d6de12aaecfc50adba35dc089
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-12 02:36:47 +0530

    refactor: offload work to the web worker

--- src/App.vue
+++ src/App.vue
@@ -11,29 +11,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { defineComponent, onMounted, ref } from 'vue';
+import myWorker from './plugins/worker';
 
 export default defineComponent({
   name: 'App',
   setup() {
     // app state
-    const ffmpeg = createFFmpeg({
-      log: true,
-    });
-    const message = ref('Click Start to Transcode');
+    let message = ref('Click Start to Transcode');
     let video = ref(null);
     const file = process.env.NODE_ENV === 'production' ? '/vue-app/flame.avi' : '/flame.avi';
+
+    onMounted(() => {
+      myWorker.worker.onmessage = ({ data }) => {
+        message.value = data.message;
+        video.value = data.video
+      };
+    })
     // methods
     async function transcode() {
-      message.value = 'Loading ffmeg-core.js';
-      await ffmpeg.load();
-      message.value = 'Start transcoding';
-      ffmpeg.FS('writeFile', 'test.avi', await fetchFile(file));
-      await ffmpeg.run('-i', 'test.avi', 'test.mp4');
-      message.value = 'Complete transcoding';
-      const data = ffmpeg.FS('readFile', 'test.mp4');
-      video.value = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+      myWorker.send({
+        sleep: 10,
+        data: file
+      });
     }
     return {
       video,
