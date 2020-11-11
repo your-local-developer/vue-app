75ad59fe6f7bb5eb5adf077cabd526548556ad6f
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-12 02:37:04 +0530

    chore(deps): add web worker using WorkerPlugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+const WorkerPlugin = require('worker-plugin');
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new WorkerPlugin({
+        // use "self" as the global object when receiving hot updates.
+        globalObject: 'self', // <-- this is the default value
+      }),
+    ],
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-app/'
     : '/'
